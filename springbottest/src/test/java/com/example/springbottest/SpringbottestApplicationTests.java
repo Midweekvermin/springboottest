ffdc5233e1dd14323c129d365541583f96036569
midweekvermin <midweekvermin@gmail.com>
2024-06-17 13:58:22 -0500

    correction

---
+++ springbottest/src/test/java/com/example/springbottest/SpringbottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbottestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
